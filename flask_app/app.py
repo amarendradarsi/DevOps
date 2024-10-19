7aaf6a37394b77b8366bc3cc51d2e1f434a8788c
amarendradarsi <darsiamarendra@gmail.com>
2024-10-18 20:29:48 -0400

    app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,43 +1,46 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 import urllib.parse
+from dotenv import load_dotenv  
+import os  
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# MongoDB connection configuration
-username = 'darsiamarendra'
-password = 'Amarendra123'  # Original password
-encoded_password = urllib.parse.quote(password)  # URL-encode the password
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
 
-# Initialize the MongoDB client and collection
+encoded_password = urllib.parse.quote(MONGODB_PASSWORD)
+
+
 client = None
 db = None
 products_collection = None
 
-# Attempt to connect to MongoDB
 try:
-    client = MongoClient(f'mongodb+srv://darsiamarendra:Amarendra123 @devops.agzoj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps')
+    
+    client = MongoClient(f'mongodb+srv://{MONGODB_USERNAME}:{encoded_password}@devops.agzoj.mongodb.net/shop_db?retryWrites=true&w=majority')
+    
     db = client['shop_db']
-    products_collection = db['products']  # This must be defined after the successful connection
+    products_collection = db['products']  
     print("Connected to MongoDB successfully")
 
 except Exception as e:
     print("Could not connect to MongoDB:", e)
 
-# Route for the homepage
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Route for the products page
 @app.route('/products')
 def products():
-    # Check if products_collection is defined before accessing it
+   
     if products_collection is None:
         return "Error: MongoDB connection not established.", 500
 
     try:
-        products_list = list(products_collection.find({}))  # Fetch products from the collection
+        products_list = list(products_collection.find({}))  
         return render_template('products.html', products=products_list)
     except Exception as e:
         print("Error fetching products:", e)
